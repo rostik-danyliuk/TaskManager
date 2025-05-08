da2078a3eb65a1a3b79fdc48c5c52da07fbf1905
rostik-danyliuk <danyliuk.rostyslav@chnu.edu.ua>
2025-05-08 21:07:12 +0300

    fix error 2

--- src/test/java/com/example/taskmanagerproject/TaskManagerProjectApplicationTests.java
+++ src/test/java/com/example/taskmanagerproject/TaskManagerProjectApplicationTests.java
@@ -2,12 +2,27 @@ package com.example.taskmanagerproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-class TaskManagerProjectApplicationTests {
+@Testcontainers
+public class TaskManagerProjectApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Test
     void contextLoads() {
+        // просто перевіряє, чи контекст стартує
     }
-
 }
+
